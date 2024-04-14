fdda99d75c7b86603f4e98954e8c67ccbf51ebcc
Luiz Henrique de Sousa <luiz.sousa26@outlook.com>
2024-04-14 16:46:02 -0300

    Aplicação final pronta ajustes

--- src/main/java/br/com/lhlibrarymanagement/repository/CategoriaRepository.java
+++ src/main/java/br/com/lhlibrarymanagement/repository/CategoriaRepository.java
@@ -9,15 +9,8 @@ import org.springframework.stereotype.Repository;
 import br.com.lhlibrarymanagement.model.Categoria;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-	
-	List<Categoria> findByNomeContainingIgnoreCase(String nome);
-	
-	@Query("select c from Categoria c order by c.id asc")
-	List<Categoria> findAllCategoriaOrdenadaPeloId();
-	
-	Categoria findByNome(String nome);
-	
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
 	@Query(value = "select ct.id, ct.nome from categoria ct", nativeQuery = true)
 	List<Categoria> findAll();
 }
